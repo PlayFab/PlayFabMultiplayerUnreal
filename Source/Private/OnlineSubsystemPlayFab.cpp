3b3a40028fac2fead694f31f19b065772237ed13
bschatt <44769431+bschatt@users.noreply.github.com>
2024-06-13 14:01:53 +0200

    Update NetworkState in OnNetworkDestroyed instead of OnLeaveNetworkCompleted

--- Source/Private/OnlineSubsystemPlayFab.cpp
+++ Source/Private/OnlineSubsystemPlayFab.cpp
@@ -1099,8 +1099,17 @@ void FOnlineSubsystemPlayFab::OnLeaveNetworkCompleted(const PartyStateChange* Ch
 	const PartyLeaveNetworkCompletedStateChange* Result = static_cast<const PartyLeaveNetworkCompletedStateChange*>(Change);
 	if (Result)
 	{
-		NetworkState = EPlayFabPartyNetworkState::NoNetwork;
-		Network = nullptr;
+		if (Result->result == PartyStateChangeResult::Succeeded)
+		{
+			// According to the documentation:
+			// An associated PartyNetworkDestroyedStateChange will be generated before this state change is generated. 
+			// https://learn.microsoft.com/en-us/gaming/playfab/features/multiplayer/networking/reference/structs/partyleavenetworkcompletedstatechange
+			if (ensure(NetworkState != EPlayFabPartyNetworkState::NoNetwork || Network != nullptr))
+			{
+				NetworkState = EPlayFabPartyNetworkState::NoNetwork;
+				Network = nullptr;
+			}
+		}
 	}
 }
 
@@ -1112,6 +1121,9 @@ void FOnlineSubsystemPlayFab::OnNetworkDestroyed(const PartyStateChange* Change)
 	if (Result)
 	{
 		UE_LOG_ONLINE(Warning, TEXT("FOnlineSubsystemPlayFab::OnNetworkDestroyed: PlayFab Party network was destroyed with reason code %d"), Result->reason);
+		
+		NetworkState = EPlayFabPartyNetworkState::NoNetwork;
+		Network = nullptr;
 
 		FPlayFabSocketSubsystem* SocketSubsystem = static_cast<FPlayFabSocketSubsystem*>(ISocketSubsystem::Get(PLAYFAB_SOCKET_SUBSYSTEM));
 		if (SocketSubsystem == nullptr)
